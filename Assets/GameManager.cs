88d3e24faefe0af4cb7e197b7680bb75b3f8e24b
Jake Hills <jhills0022@yahoo.com>
2019-08-16 00:12:36 -0400

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    bool canOpenPack = true;
+    [SerializeField] GameObject[] cards;
+    [SerializeField] Transform[] spawnPoints;
+    CardPackAnim cardPack;
+    Canvas canvas;
+    int i = 0;
+    int j = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cardPack = FindObjectOfType<CardPackAnim>();
+        canvas = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnCards()
+    {
+        if (canOpenPack == true)
+        {
+            foreach (GameObject card in cards)
+            {
+                if (i <= cards.Length - 1)
+                {
+                    if (j <= spawnPoints.Length - 1)
+                    {
+                        var spawnCard = Instantiate(card, spawnPoints[j].transform.position, Quaternion.identity) as GameObject;
+                        spawnCard.transform.SetParent(canvas.transform);
+                        var playCardAnim = spawnCard.GetComponent<Animator>();
+                        playCardAnim.Play("newCard");
+
+                        i++;
+                        j++;
+                    }
+                }
+                
+            }
+        }
+    }
+
+    public void PlayPackAnim()
+    {
+
+    }
+}
---
+++ Assets/Scripts/OnMouseOverBounce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnMouseOverBounce : MonoBehaviour, IPointerEnterHandler
+{
+
+    private Animator buttonAnim;
+    private Animator cardPackAnim;
+    CardPackAnim cardPackObject;
+
+	// Use this for initialization
+	void Start ()
+    {    
+        buttonAnim = GetComponent<Animator>();
+        cardPackObject = FindObjectOfType<CardPackAnim>();
+        cardPackAnim = cardPackObject.GetComponent<Animator>();
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonAnim.Play("Bounce");
+        cardPackAnim.Play("BreakOpen");
+    }
+
+   public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonAnim.Play("Idle");
+    }
+
+}
